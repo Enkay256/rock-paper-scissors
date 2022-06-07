340566cad2d03ca688fe75cf32ef7b2f0df76a46
unknown <nkemflorence2020@gmail.com>
2022-06-07 12:33:47 +0100

    a rock-paper-scissors game

---
+++ rockpaperscissors.py
@@ -0,0 +1,68 @@
+from ast import Break
+import random
+from re import U
+
+
+while True:
+ choices = ["r", "p", "s"]
+ computer = random.choice(choices)
+ player = None
+
+ while player not in choices:
+       player = input("pick an option from r, p s: ").lower()
+
+ if player == computer:
+    print("computer: ",computer)
+    print("player: ",player)
+    print("It's a tie")
+    
+    
+
+ elif player == "r":
+    if computer == "p":
+       print("computer: ", computer)
+       print("player: ", player)
+       print("You Lose")
+    if computer == "s":
+       print("computer: ", computer)
+       print("player: ", player)
+       print("You Win")
+    
+    break
+ 
+
+    
+
+ elif player == "s":
+    if computer == "r":
+       print("computer: ", computer)
+       print("player: ", player)
+       print("You Lose")
+    if computer == "p":
+       print("computer: ", computer)
+       print("player: ", player)
+       print("You Win")
+
+    break
+
+ elif player == "p":
+    if computer == "s":
+       print("computer: ", computer)
+       print("player: ", player)
+       print("You Lose")
+    if computer == "r":
+       print("computer: ", computer)
+       print("player: ", player)
+       print("You Win")
+    
+
+    break
+ 
+    
+             
+
+
+        
+
+
+        
